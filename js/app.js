7b184b0716bb26d8bfd016f3d5362dd091c2f526
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-05-12 13:42:21 +0100

    added 'nav a' and 'section a' in app.js to enable the menu scrolling on mobile

--- js/app.js
+++ js/app.js
@@ -2,10 +2,31 @@ console.log('Yo');
 
 $(()=> {
 
-  const $links = $('a');
+  const $window = $(window);
+  const $links = $('nav a');
+  const $links2 = $('section a');
+  const $menu = $('.menu');
+
   $links.on('click', scrollToSection);
+  $links2.on('click', scrollToSection2);
+  $menu.on('click', toggleMenu);
+
+  function toggleMenu(){
+    $('.dropdown').slideToggle();
+  }
 
   function scrollToSection() {
+    const section = $(this).attr('href'); // whatever is clicked on eg #location
+    $('body').animate( {
+      scrollTop: $(section).offset().top
+    }, 1000, () => {
+      if ($window.width() < 575) {
+        $('.dropdown').slideToggle();
+      }
+    });
+  }
+
+  function scrollToSection2() {
     const section = $(this).attr('href');
     $('body').animate({ scrollTop: $(`${section}`).offset().top}, 1000);
   }
